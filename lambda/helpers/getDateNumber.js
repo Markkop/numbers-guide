a606df46662d4c185449213f12373e6835dd9a6f
Mark Kop <marcelokopmann@gmail.com>
2021-01-24 15:11:43 -0300

    feat: get a single digit number

--- lambda/helpers/getDateNumber.js
+++ lambda/helpers/getDateNumber.js
@@ -1,7 +1,28 @@
+
+/**
+ * Splits a number received as text and sum its
+ * digits until there's only one
+ * @param { String } numberText 
+ * @returns { String }
+ */
+function splitAndSumNumbers(numberText) {
+    const splittedNumbers = numberText.split('').map(Number)
+    const sum = String(splittedNumbers.reduce((sum, number) => sum + number, 0))
+    if (sum.length === 1) {
+        return sum
+    }
+    return splitAndSumNumbers(sum)
+}
+
+/**
+ * Gets a single number for a given string date
+ * @param { String } date such as 2021-12-19
+ * @returns { Number } 
+ */
 function getDateNumber(date) {
-    const dateNumbers = date.replace(/\D/g, '').split('').map(Number)
-    const reducedNumber = dateNumbers.reduce((sum, number) => sum + number, 0)
-    return reducedNumber   
+    const dateNumbers = date.replace(/\D/g, '')
+    const sum = splitAndSumNumbers(dateNumbers)
+    return Number(sum)   
 }
 
 module.exports = getDateNumber
--- lambda/test/getDateNumber.test.js
+++ lambda/test/getDateNumber.test.js
@@ -5,4 +5,9 @@ describe('getDateNumber', () => {
         const number = getDateNumber('2021-12-01')
         expect(number).toBe(9)
     })
+
+    it('gets the number for 2021-12-19', () => {
+        const number = getDateNumber('2021-12-09')
+        expect(number).toBe(8)
+    })
 })
